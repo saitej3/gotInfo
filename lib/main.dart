d4995265724c34a1f58882b16b962afadbcc344d
khkr <32921476+khkred@users.noreply.github.com>
2023-07-01 09:37:19 -0700

    Firebase App updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:got_info/src/got_info_app.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ProviderScope(child: GotInfoApp()));
 }
--- lib/src/features/authentication/presentation/sign_in_page.dart
+++ lib/src/features/authentication/presentation/sign_in_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -50,7 +49,7 @@ class _LoginPageWidgetState extends ConsumerState<SignInPage> {
                   child: Column(
                     children: [
                       Image.asset(
-                        'images/logo_transparent.png',
+                        'assets/images/logo_transparent.png',
                         height: 200,
                         width: 200,
                       ),
